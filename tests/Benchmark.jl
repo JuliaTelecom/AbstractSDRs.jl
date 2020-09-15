0eaa2eb9844b62f3f962a2b578c57c0df1c344b6
Robin Gerzaguet <robin.gerzaguet@irisa.fr>
2020-09-15 16:40:05 +0200

    Adding benchmark tools

--- tests/Benchmark.jl
+++ tests/Benchmark.jl
@@ -16,14 +16,14 @@ end
 """
 Main call to monitor Rx rate
 """
-function main(sdr,samplingRate)	
+function main(radio,samplingRate)	
 	# ---------------------------------------------------- 
 	# --- Physical layer and RF parameters 
 	# ---------------------------------------------------- 
 	# --- Create the radio object in function
 	carrierFreq		= 770e6;		
 	gain			= 50.0; 
-	radio			= openSDR(sdr,carrierFreq,samplingRate,gain); 
+	updateSamplingRate!(radio,samplingRate);
 	# --- Print the configuration
 	print(radio);
 	# --- Init parameters 
@@ -44,7 +44,7 @@ function main(sdr,samplingRate)
 		p = recv!(sig,radio);
 		# # --- Ensure packet is OK
 		# err 	= getError(radio);
-		(p != pInit) && (print("."));
+		# (p != pInit) && (print("."));
 		# --- Update counter
 		nS		+= p;
 		# --- Interruption 
@@ -58,7 +58,42 @@ function main(sdr,samplingRate)
 	radioRate	  = radio.samplingRate;
     effectiveRate = getRate(timeInit,timeFinal,nS);
 	# --- Free all and return
-	close(radio);
 	return (radioRate,effectiveRate);
-    end
+end
+
+
+struct Res 
+	radio::String;
+	carrierFreq::Float64;
+	gain::Float64;	
+	rateVect::Array{Float64};
+	effectiveRate::Array{Float64};
+	radioRate::Array{Float64};
+end
+export Res
+
+
+function bench()
+	# --- Set priority 
+	# --- Configuration
+	radioName 		= "e310";
+	carrierFreq		= 770e6;		
+	gain			= 50.0; 
+	# rateVect	= [1e3;100e3;500e3;1e6:1e6:8e6;16e6;32e6;64e6;80e6;100e6;200e6];
+	rateVect	= [1e3;100e3;500e3;1e6:1e6:8e6;16e6];
+	effectiveRate	= zeros(Float64,length(rateVect));
+	radioRate	= zeros(Float64,length(rateVect));
+	# --- Setting a very first configuration 
+	global radio = openSDR(radioName,carrierFreq,1e6,gain;ip="192.168.10.12"); 
+	for (iR,targetRate) in enumerate(rateVect)
+		(rR,eR) = main(radio,targetRate);
+		radioRate[iR] = rR;
+		effectiveRate[iR] = eR;
+	end
+	close(radio);
+	strucRes  = Res(radioName,carrierFreq,gain,rateVect,effectiveRate,radioRate);
+	# @save "benchmark_UHD.jld2" res;
+	return strucRes;
+end
+
 end
